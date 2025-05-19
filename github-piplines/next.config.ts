6c86a93d142800a4c1b1e03e3b6ad2edddc3fd22
Imtiyaz-Rahman <imtiyazr@outlook.com>
2025-05-19 22:31:15 +0100

    initial commit

--- github-piplines/next.config.ts
+++ github-piplines/next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   basePath: "/github-piplines", // Could cause a problem so check
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
+
+  
+    images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
